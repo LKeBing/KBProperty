35dd89cfc2d664ec5fcd9cb89d335b02bed752bb
kebing.lin <lkb@sxw.cn>
2021-03-30 10:45:52 +0800

    属性定义支持nullable

--- KBProperty/KBProperty.h
+++ KBProperty/KBProperty.h
@@ -9,42 +9,51 @@
 #ifndef KBProperty_h
 #define KBProperty_h
 
-#define KBStrongProperty            @property (nonatomic, strong)
-#define KBStrongPropertyWithClass(className)            KBStrongProperty className *
-#define KBStrongIdProperty            KBStrongProperty id
-#define KBAssignProperty            @property (nonatomic, assign)
-#define KBCopyProperty              @property (nonatomic, copy)
-#define KBReadOnlyStrongProperty    @property (nonatomic, strong, readonly)
+#define KBAssignProperty                        @property (nonatomic, assign)
+#define KBCopyProperty                          @property (nonatomic, copy, nullable)
+#define KBStrongProperty                        @property (nonatomic, strong, nullable)
+#define KBWeakProperty                          @property (nonatomic, weak, nullable)
 
-#define KBStringProperty    KBCopyProperty NSString *
-#define KBReadOnlyStringProperty    @property (nonatomic, copy, readonly) NSString *
-#define KBDataProperty      KBStrongProperty NSData *
-#define KBNumberProperty    KBStrongProperty NSNumber *
-#define KBViewProperty      KBStrongProperty UIView *
-#define KBColorProperty     KBStrongProperty UIColor *
-#define KBIntegerProperty   KBAssignProperty NSInteger
-#define KBFloatProperty     KBAssignProperty CGFloat
-#define KBBOOLProperty      KBAssignProperty BOOL
-#define KBImageProperty     KBStrongProperty UIImage *
-#define KBInsetsProperty    KBAssignProperty UIEdgeInsets
+#define KBStrongReadProperty                    @property (nonatomic, strong, readonly)
+#define KBCopyReadProperty                      @property (nonatomic, copy, readonly)
+
+
+#define KBStrongPropertyWithClass(class)        KBStrongProperty class *
+
+
+#define KBStringProperty                        KBCopyProperty NSString *
+#define KBDataProperty                          KBStrongProperty NSData *
+#define KBNumberProperty                        KBStrongProperty NSNumber *
+#define KBViewProperty                          KBStrongProperty UIView *
+#define KBColorProperty                         KBStrongProperty UIColor *
+#define KBImageProperty                         KBStrongProperty UIImage *
+#define KBIntegerProperty                       KBAssignProperty NSInteger
+#define KBFloatProperty                         KBAssignProperty CGFloat
+#define KBBOOLProperty                          KBAssignProperty BOOL
+#define KBInsetsProperty                        KBAssignProperty UIEdgeInsets
+#define KBStrongIdProperty                      KBStrongProperty id
+
+
+#define KBArrayProperty                             KBStrongProperty NSArray *
+#define KBMutableArrayProperty                      KBStrongProperty NSMutableArray *
+#define KBArrayPropertyWithType(ObjType)            KBStrongProperty NSArray<ObjType *> *
+#define KBMutableArrayPropertyWithType(ObjType)     KBStrongProperty NSMutableArray<ObjType *> *
+
+
+#define KBDictionaryProperty                            KBStrongProperty NSDictionary *
+#define KBMutableDictionaryProperty                     KBStrongProperty NSMutableDictionary *
+#define KBDictionaryPropertyWithType(key,value)         KBStrongProperty NSDictionary<key *,value *> *
+#define KBMutableDictionaryPropertyWithType(key,value)  KBStrongProperty NSMutableDictionary<key *,value *> *
+
 
 #define KBMutableSetPropertyWithType(ObjType)           KBStrongProperty NSMutableSet<ObjType *> *
 #define KBMutableOrderedSetPropertyWithType(ObjType)    KBStrongProperty NSMutableOrderedSet<ObjType *> *
 
 
-#define KBArrayProperty         KBStrongProperty NSArray *
-#define KBMutableArrayProperty  KBStrongProperty NSMutableArray *
-#define KBArrayPropertyWithType(ObjType)         KBStrongProperty NSArray<ObjType *> *
-#define KBMutableArrayPropertyWithType(ObjType)  KBStrongProperty NSMutableArray<ObjType *> *
+#define KBBlockProperty                         KBCopyProperty
 
 
-#define KBDictionaryProperty        KBStrongProperty NSDictionary *
-#define KBMutableDictionaryProperty KBStrongProperty NSMutableDictionary *
-#define KBDictionaryPropertyWithType(key,value)        KBStrongProperty NSDictionary<key *,value *> *
-#define KBMutableDictionaryPropertyWithType(key,value) KBStrongProperty NSMutableDictionary<key *,value *> *
-
-#define KBBlockProperty         KBCopyProperty
-#define KBDelegateProperty(ProtocolType)     @property (nonatomic, weak, nullable) id<ProtocolType>
-#define KBDelegatesProperty(ProtocolType)    KBStrongProperty NSMutableSet<id<ProtocolType>> *
+#define KBDelegateProperty(ProtocolType)        KBWeakProperty id<ProtocolType>
+#define KBDelegatesProperty(ProtocolType)       KBStrongProperty NSMutableSet<id<ProtocolType>> *
 
 #endif /* KBProperty_h */
